7cb733fb2f3ec6dca7d5a23dcdd4e1e24b17b15f
Gašpr <96392498+Gaspr21@users.noreply.github.com>
2025-10-18 20:20:40 +0200

    add update readme file

---
+++ .github/update_readme.py
@@ -0,0 +1,47 @@
+import datetime
+import re
+
+
+README_PATH = "README.md"
+
+def GenerateAJoke():
+    # generate a joke automatically using ai
+    # THIS IS WHERE YOU PUT THE AI CODE
+    print("HELLO WORLD !")
+    return ("famous artist")
+
+
+def Update_dynamic_section():
+    """ Generates a dynamic text for the README file.  """
+    
+    startMarker = "<!--START_DYNAMIC-->"
+    endMarker = "<!--END_DYNAMIC-->"
+    current_time = datetime.datetime.now().strftime(f"%d-%m-%Y %H:%M:%S") 
+
+    jokeOfTheDay = GenerateAJoke()
+    
+   
+    with open(README_PATH, "r", encoding="utf-8") as f:
+        readme = f.read()
+
+    dynamic_content = f"{startMarker}\n{GenerateAJoke()}\n{endMarker}"
+
+    if re.search(f"{startMarker}[\\s\\S]*{endMarker}", readme):
+        readme = re.sub(f"{startMarker}[\\s\\S]*{endMarker}", dynamic_content, readme)
+    else:
+        readme += f"\n\n{dynamic_content}"
+
+    with open(README_PATH, "w", encoding="utf-8") as f:
+        f.write(readme)
+
+    print("✅ README updated successfully!")
+
+    return (
+        f"_{jokeOfTheDay}_\n"
+        f"### ✨ Auto-updated Info\n"
+        f"🕒 Updated on: **{current_time}**  \n"
+    )
+
+
+if __name__ == "__main__":
+    Update_dynamic_section()
--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 - 💾 I like solving problems and turning ideas into real, lasting solutions. Coding is a great tool to do that. It gives me the opportunity to think creatively, collaborate with others, and keep learning every day. I want to build a career where I can grow, make an impact through my work, and be part of a team that inspires and supports one another.
 - 🌱 I’m currently working on improving my skills and adding to my knowledge of ReactJS, Typescript and containerization.
 - 📫 Would like to reach out? You can do it through [LinkedIn](https://www.linkedin.com/in/ga%C5%A1pr-hace-71a912250/) it might take 3-5 business days... 
-- ⚡ Joke of the day: I'm working on it...
+- ⚡ Joke of the day: 
+    <!--START_DYNAMIC-->
+    <!--END_DYNAMIC-->
 
